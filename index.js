68d350265be6de86470f54e17461ce1b44fa34c2
Daniel Gorter <danieljgorter@gmail.com>
2019-01-06 20:43:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var sentences = [];
+  for (let i = 0; i < musicians.length; i++){
+    sentences.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return sentences;
+}
+
+function johnLennonFacts(facts){
+  var superFacts = [];
+  var i = 0;
+  while (i < facts.length){
+    superFacts.push(`${facts[i]}!!!`);
+    i++;
+  }
+  return superFacts;
+}
